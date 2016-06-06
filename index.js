75c0e78d2f78e654d5c03328aac2c176d27aa2cc
Ashley Taylor <ashleytaylor14@gmail.com>
2016-06-06 08:17:43 +0100

    Removed function invocation. Exports app

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 var express = require('express');
 var app = express();
 
-module.exports = function onAuthExport() {
-	app.post('/auth/login', require('./components/login'));
-	app.post('/auth/logout', require('./components/logout'));
-	app.post('/auth/register', require('./components/register'));
-	app.post('/auth/password-forgot', require('./components/password-forgot'));
+app.post('/auth/login', require('./components/login'));
+app.post('/auth/logout', require('./components/logout'));
+app.post('/auth/register', require('./components/register'));
+app.post('/auth/password-forgot', require('./components/password-forgot'));
 
-	app.get('/auth/password-reset', require('./components/password-reset').get);
-	app.post('/auth/password-reset', require('./components/password-reset').post);
+app.get('/auth/password-reset', require('./components/password-reset').get);
+app.post('/auth/password-reset', require('./components/password-reset').post);
 
-	return app;
-};
+module.exports = app;
